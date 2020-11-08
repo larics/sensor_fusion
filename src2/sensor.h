30f3dc92e5c19b68af0f30a183b87d48483f2b0d
Kovac0903 <marinkovac0903@gmail.com>
2020-11-08 22:34:09 +0100

    Add init flag for sensor class

--- src2/sensor.h
+++ src2/sensor.h
@@ -8,13 +8,18 @@
 #include "nav_msgs/Odometry.h"
 using namespace Eigen;
 
+/*
+ * We define sensor subscriber and when an odometry msg comes we
+ * update the es-ekf state.
+ */
+
 class Sensor{
 		// subscriber for sensor odometry type msg
 		ros::Subscriber sensor_sub;
 		ros::NodeHandle node_handle_;
 		SensorParams params_;
 		EsEkf* es_ekf_;
-		bool first_measurement;
+		bool init;
 
 public:
 		Sensor(SensorParams params, EsEkf* es_ekf):
@@ -22,19 +27,20 @@ public:
 		{
 			// if its the first measurement update the intial pose
 			// and dont call measurement update
-			first_measurement = false;
+			init = false;
 			sensor_sub = node_handle_.subscribe(params_.topic, 1,
 																					&Sensor::callback_sensor, this);
 
 		}
+		bool isInit(){return init;}
 		void callback_sensor(const nav_msgs::OdometryPtr& msg){
 			Matrix<double, 3, 1> data;
-		data << msg->pose.pose.position.x,
-						msg->pose.pose.position.y,
-						msg->pose.pose.position.z;
-		data = params_.rotation_mat * data + params_.translation;
+			data << msg->pose.pose.position.x,
+							msg->pose.pose.position.y,
+							msg->pose.pose.position.z;
+			//data = params_.rotation_mat * data + params_.translation;
 
-			if(first_measurement && !(es_ekf_->isInit())){
+			if(!init){
 				//Set initial state
 				//TODO this has to be done in one step with a
 				// a defined structure for to es-ekf state.
@@ -48,12 +54,13 @@ public:
 													msg->pose.pose.orientation.y,
 													msg->pose.pose.orientation.z});
 
-				first_measurement = false;
+				init = true;
 			}
-			else{
+			else if(es_ekf_->isInit()){
 				es_ekf_->measurement_update(params_.cov.R,
 																		data);
 			}
+			else ROS_INFO("Sensor Measurement: Es-Ekf not initialized");
 		}
 
 		~Sensor() {
