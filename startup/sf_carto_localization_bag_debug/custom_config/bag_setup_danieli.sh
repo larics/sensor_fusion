c59376c62b6a452d8caad8e099d612a55738f969
Robert Milijaš <robert.milijas@fer.hr>
2021-08-19 13:37:29 +0200

    Add localization session

---
+++ startup/sf_carto_localization_bag_debug/custom_config/bag_setup_danieli.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export BAG_NAME=$HOME/FER/cartographer_experiments/Danieli/Localization/localization_2.bag
+export CONFIGURATION_BASENAME=localization_pozyx.lua
+export CONFIGURATION_DIRECTORY=$(pwd)/custom_config
+export CARTO_URDF=$(rospack find uav_ros_general)/urdf/ouster-imu.urdf.xacro
+export PBSTREAM_NAME=$HOME/FER/cartographer_experiments/Danieli/Sensor_fusion/maps/sensor_fusion_1.pbstream
+
+# Starter parameters
+export ENABLE_STARTER=true
+export USE_FIXED_YAW=true
+export FIXED_YAW=1.57
+
+# Define bag topics
+export BAG_POINT_TOPIC=/os_cloud_node/points
+export BAG_IMU_TOPIC=/danieli2/mavros/imu/data
+export BAG_TRANSFORM_TOPIC=/pozyx/measured
+export BAG_CAMERA_ODOM_TOPIC=/camera/odom/sample
+
+# Define cartographer topics
+export POINT_TOPIC=/$UAV_NAMESPACE/os_cloud_node/points
+export IMU_TOPIC=/$UAV_NAMESPACE//mavros/imu/data
+export TRANSFORM_TOPIC=/$UAV_NAMESPACE/pozyx/measured
+export CAMERA_ODOM_TOPIC=/$UAV_NAMESPACE/camera/odom/sample
---
+++ startup/sf_carto_localization_bag_debug/custom_config/ouster_pozyx.lua
@@ -0,0 +1,96 @@
+-- Copyright 2016 The Cartographer Authors
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+include "map_builder.lua"
+include "trajectory_builder.lua"
+namespace = os.getenv("UAV_NAMESPACE")
+
+options = {
+  map_builder = MAP_BUILDER,
+  trajectory_builder = TRAJECTORY_BUILDER,
+  map_frame =  namespace.."/map",
+  tracking_frame =  namespace.."/base_link",
+  published_frame =  namespace.."/base_link",
+  odom_frame = namespace.."/odom",
+  provide_odom_frame = false,
+  publish_frame_projected_to_2d = false,
+  use_pose_extrapolator = true,
+  use_odometry = false,
+  use_nav_sat = false,
+  use_position_sensor = true, -- use POZYX
+  position_translation_weight = 1.2, -- 1e0, ----------------- POZYX
+  nav_sat_use_predefined_enu_frame = false,
+  nav_sat_predefined_enu_frame_lat_deg = 45.813902,
+  nav_sat_predefined_enu_frame_lon_deg = 16.038766,
+  nav_sat_predefined_enu_frame_alt_m = 168.259294525,
+  nav_sat_translation_weight = 1.,
+  nav_sat_inverse_covariance_weight = 0.4,
+  nav_sat_inverse_covariance_bias = 0,
+  use_landmarks = false,
+  num_laser_scans = 0,
+  num_multi_echo_laser_scans = 0,
+  num_subdivisions_per_laser_scan = 1,
+  num_point_clouds = 1,
+  lookup_transform_timeout_sec = 0.2,
+  submap_publish_period_sec = 0.3,
+  pose_publish_period_sec = 2e-2,
+  trajectory_publish_period_sec = 1e0,
+  rangefinder_sampling_ratio = 1.,
+  odometry_sampling_ratio = 1.,
+  fixed_frame_pose_sampling_ratio = 1.,
+  imu_sampling_ratio = 1.,
+  landmarks_sampling_ratio = 1.,
+}
+
+TRAJECTORY_BUILDER.collate_fixed_frame = false -- test this
+
+TRAJECTORY_BUILDER_3D.num_accumulated_range_data = 1
+TRAJECTORY_BUILDER_3D.ceres_scan_matcher.translation_weight = 4 -- 4 -- 8 --5.5
+TRAJECTORY_BUILDER_3D.ceres_scan_matcher.translation_weight_z = 4 -- 4 -- 8 --5.5
+TRAJECTORY_BUILDER_3D.ceres_scan_matcher.rotation_weight = 150 -- 4 -- 8 -- 5
+TRAJECTORY_BUILDER_3D.submaps.high_resolution = 0.2
+TRAJECTORY_BUILDER_3D.submaps.low_resolution = 0.45
+TRAJECTORY_BUILDER_3D.submaps.high_resolution_max_range = 40.
+TRAJECTORY_BUILDER_3D.submaps.num_range_data = 160.
+TRAJECTORY_BUILDER_3D.high_resolution_adaptive_voxel_filter = {
+    max_length = 2.,
+    min_num_points = 150,
+    max_range = 30.,  -- default 50
+}
+
+TRAJECTORY_BUILDER_3D.low_resolution_adaptive_voxel_filter = {
+    max_length = 4.,
+    min_num_points = 200,
+    max_range = 70,
+}
+
+MAP_BUILDER.use_trajectory_builder_3d = true
+MAP_BUILDER.num_background_threads = 3 -- default 3
+POSE_GRAPH.optimization_problem.huber_scale =  1 -- 5e2
+POSE_GRAPH.optimize_every_n_nodes = 320 --480
+POSE_GRAPH.constraint_builder.sampling_ratio = 0.15 -- less = faster algo default 0.3
+POSE_GRAPH.constraint_builder.max_constraint_distance = 15 -- default 15
+POSE_GRAPH.optimization_problem.ceres_solver_options.max_num_iterations = 25  -- default 50
+POSE_GRAPH.constraint_builder.min_score = 0.55  -- default 0.55
+POSE_GRAPH.constraint_builder.global_localization_min_score = 0.66    -- og 0.6
+POSE_GRAPH.optimization_problem.rotation_weight = 0
+POSE_GRAPH.optimization_problem.acceleration_weight = 1e2
+-- ovi dolje su og 10
+POSE_GRAPH.constraint_builder.ceres_scan_matcher_3d.translation_weight = 5
+POSE_GRAPH.constraint_builder.ceres_scan_matcher_3d.translation_weight_z = 5
+POSE_GRAPH.constraint_builder.ceres_scan_matcher_3d.rotation_weight = 5
+POSE_GRAPH.optimization_problem.nav_sat_huber_scale = 0
+POSE_GRAPH.optimization_problem.enable_nav_sat_huber_loss = false
+
+return options
