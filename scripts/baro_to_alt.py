b084b122cec3b6ea59408d8aa54b52cc05c46f9c
Marijana <marijana.peti@fer.hr>
2023-10-07 13:45:57 +0200

    Add dummy topic with latch = True to baro_to_alt.py

--- scripts/baro_to_alt.py
+++ scripts/baro_to_alt.py
@@ -12,9 +12,11 @@ class Baro2Alt():
 	def __init__(self):
 		rospy.Subscriber("mavros/imu/static_pressure", FluidPressure, self.baro_callback)
 		self.pub_height = rospy.Publisher('height_from_pressure', Odometry, queue_size=10)
+		self.pub_dummy_height = rospy.Publisher('dummy_altitude', Odometry, queue_size=10, latch=True)
 		self.base_pressure = 0
 		self.height = 0
 		self.first_pressure_callback = 1
+		self.first_converted_baro_to_alt = 1
 
 	def baro_callback(self, msg):
 		pressure = msg.fluid_pressure
@@ -22,11 +24,14 @@ class Baro2Alt():
 			self.base_pressure = pressure
 			self.first_pressure_callback = 0
 		self.height = self.baro_to_alt(pressure, self.base_pressure)
-		print(self.height)
 		height_msgs = Odometry()
 		height_msgs.header = msg.header
 		height_msgs.pose.pose.position.z = self.height
-		self.pub_height.publish(height_msgs)
+		if self.first_converted_baro_to_alt:
+			self.first_converted_baro_to_alt = 0
+			self.pub_dummy_height.publish(height_msgs)
+		else:
+			self.pub_height.publish(height_msgs)
 
 	def baro_to_alt(self, pressure, base_pressure):
 		return K1*BASE_TEMPERATURE*(1.0-math.exp(K2*math.log(pressure/base_pressure)))
