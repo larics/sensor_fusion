5a8d78221a5a5ba1bf35b34f85918ed99bcd4a27
marijana23 <marijana.peti@fer.hr>
2023-10-09 12:12:42 +0200

    [baro_to_alt] Remove latch, add location trigger subscriber

--- scripts/baro_to_alt.py
+++ scripts/baro_to_alt.py
@@ -3,6 +3,7 @@ import rospy
 import math
 from sensor_msgs.msg import FluidPressure
 from nav_msgs.msg import Odometry
+from geometry_msgs.msg import PoseWithCovarianceStamped
 
 BASE_TEMPERATURE = 296.15
 K1 = 153.8462
@@ -10,13 +11,15 @@ K2 = 0.190259
 
 class Baro2Alt():
 	def __init__(self):
-		rospy.Subscriber("mavros/imu/static_pressure", FluidPressure, self.baro_callback)
-		self.pub_height = rospy.Publisher('height_from_pressure', Odometry, queue_size=10)
-		self.pub_dummy_height = rospy.Publisher('dummy_altitude', Odometry, queue_size=10, latch=True)
 		self.base_pressure = 0
 		self.height = 0
 		self.first_pressure_callback = 1
-		self.first_converted_baro_to_alt = 1
+		self.location_triggered = False
+
+		rospy.Subscriber("mavros/imu/static_pressure", FluidPressure, self.baro_callback)
+		self.location_trigger_sub = rospy.Subscriber("location", PoseWithCovarianceStamped, self.location_trigger_cb)
+		self.pub_height = rospy.Publisher('height_from_pressure', Odometry, queue_size=10)
+		self.pub_dummy_height = rospy.Publisher('dummy_altitude', Odometry, queue_size=10)
 
 	def baro_callback(self, msg):
 		pressure = msg.fluid_pressure
@@ -27,12 +30,17 @@ class Baro2Alt():
 		height_msgs = Odometry()
 		height_msgs.header = msg.header
 		height_msgs.pose.pose.position.z = self.height
-		if self.first_converted_baro_to_alt:
-			self.first_converted_baro_to_alt = 0
-			self.pub_dummy_height.publish(height_msgs)
-		else:
-			self.pub_height.publish(height_msgs)
-
+		self.pub_height.publish(height_msgs)
+		
+		if self.location_triggered:
+			return
+		rospy.logwarn_throttle(1.0, "[Baro2Alt] - Triggering Lora with Baro!")
+		self.pub_dummy_height.publish(height_msgs)
+	
+	def location_trigger_cb(self, msg):
+		self.location_triggered = True
+		rospy.loginfo_once("[Baro2Alt] - Lora received!")
+	
 	def baro_to_alt(self, pressure, base_pressure):
 		return K1*BASE_TEMPERATURE*(1.0-math.exp(K2*math.log(pressure/base_pressure)))
 
